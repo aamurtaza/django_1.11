4279c02246c5e30903a4cb3ff8b51085f8e631ea
adnanbajwa <adnan.bajwa163@gmail.com>
2017-05-12 21:56:11 +0300

    Get Item or 404 Method

--- django_learning_project/src/posts/views.py
+++ django_learning_project/src/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,13 +9,15 @@ def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request): #retrieve
+	# instance = Post.objects.get(id=5)
+	instance = get_object_or_404(Post, id = 1)
 	context  = {
-		"title" : "Detail"
+		"title" : instance.title,
+		"instance":instance
 	}
-	return render(request, "index.html", context)
+	return render(request, "post_detail.html", context)
 
 def post_list(request): #list items
-
 	queryset = Post.objects.all()
 	context  = {
 		"queryset":queryset,
