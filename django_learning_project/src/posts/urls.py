5281fda33e8d33f6803544908d3a1e2e5742c768
adnanbajwa <adnan.bajwa163@gmail.com>
2017-05-13 18:49:37 +0300

    Get links, absosulte url, reverse method

--- django_learning_project/src/posts/urls.py
+++ django_learning_project/src/posts/urls.py
@@ -1,12 +1,20 @@
 
 from django.conf.urls import url
-from . import views
+
+from .views import (
+	post_list,
+	post_create,
+	post_detail,
+	post_update,
+	post_delete,
+	)
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^create/$', views.post_create),
-    url(r'^(?P<id>\d+)/$', views.post_detail),
-    url(r'^update/$', views.post_update),
-    url(r'^delete/$', views.post_delete),
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
+    url(r'^$', post_list, name='test'),
+    url(r'^create/$', post_create),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
+    #url(r'^posts/$', "<appname>.views.<function_name>"), 
+    # url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
 ]
