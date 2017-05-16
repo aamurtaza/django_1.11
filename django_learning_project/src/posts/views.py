d0753f017c390874db1f817e283b6126f8b113b4
adnanbajwa <adnan.bajwa163@gmail.com>
2017-05-16 21:30:09 +0300

    Update View

--- django_learning_project/src/posts/views.py
+++ django_learning_project/src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -9,48 +9,45 @@ def post_create(request):
 	form = PostForm(request.POST or None) 
 	if form.is_valid():
 		instance = form.save(commit=False)
-		print (form.cleaned_data.get("title"))
 		instance.save()
-	#not recommended
-	# if request.method == "POST":
-	# 	print (request.POST.get("content"))
-	# 	print (request.POST.get("title"))
-		# Post.objects.create(title=title) 
+		# message success
+		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
 		"form":form,
 	}
-
 	return render(request, "post_form.html", context)
 
-def post_detail(request , id=None): #retrieve
-	# instance = Post.objects.get(id=5)
+def post_detail(request , id=None):
 	instance = get_object_or_404(Post, id = id)
 	context  = {
 		"title" : instance.title,
-		"instance":instance
+		"instance":instance,
 	}
 	return render(request, "post_detail.html", context)
 
-def post_list(request): #list items
+def post_list(request):
 	queryset = Post.objects.all()
 	context  = {
 		"queryset":queryset,
 		"title" : "List"
 	}		
-	
-	# if request.user.is_authenticated():
-	# 	context  = {
-	# 		"title" : "User list"
-	# 	}
-	# else:
-	# 	context  = {
-	# 		"title" : "List"
-	# 	}
 	return render(request, "index.html", context)
-	# return HttpResponse("<h1>List</h1>")
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form":form,
+	}
+	return render(request, "post_form.html", context)
+
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
