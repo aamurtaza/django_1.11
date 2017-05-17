c13405d0656c5d3c4654bf95166e5935515f8f2f
adnanbajwa <adnan.bajwa163@gmail.com>
2017-05-17 20:54:47 +0300

    Django templates and inheritance

--- django_learning_project/src/posts/views.py
+++ django_learning_project/src/posts/views.py
@@ -1,62 +1,68 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages	
+from django.contrib import messages
 
 # Create your views here.
 from .forms import PostForm
 from .models import Post
 
-def post_create(request):
-	form = PostForm(request.POST or None) 
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		# message success
-		messages.success(request, "Post successfully created")
-		return HttpResponseRedirect(instance.get_absolute_url())
-	else:
-		messages.error(request, "Post not successfully created.")
-	context = {
-		"form":form,
-	}
-	return render(request, "post_form.html", context)
 
-def post_detail(request , id=None):
-	instance = get_object_or_404(Post, id = id)
-	context  = {
-		"title" : instance.title,
-		"instance":instance,
-	}
-	return render(request, "post_detail.html", context)
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        # message success
+        messages.success(request, "Post successfully created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Post not successfully created.")
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+
+def post_detail(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+    }
+    return render(request, "post_detail.html", context)
+
 
 def post_list(request):
-	queryset = Post.objects.all()
-	context  = {
-		"queryset":queryset,
-		"title" : "List"
-	}		
-	return render(request, "index.html", context)
+    queryset = Post.objects.all()
+    context = {
+        "queryset": queryset,
+        "title": "List"
+    }
+    return render(request, "post_list.html", context)
+
 
 def post_update(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "Post updated")
-		# For tags understanding
-		# messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
-		return HttpResponseRedirect(instance.get_absolute_url())
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Post updated")
+        # For tags understanding
+        # messages.success(request, "<a href='#'>Item</a> Saved",
+        # extra_tags='html_safe')
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = {
-		"title": instance.title,
-		"instance": instance,
-		"form":form,
-	}
-	return render(request, "post_form.html", context)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
 
 def post_delete(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	instance.delete()
-	messages.success(request, "Post successfully deteted")
-	return redirect("posts:list")
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Post successfully deteted")
+    return redirect("posts:list")
