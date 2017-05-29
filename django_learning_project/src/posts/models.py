15843a1638e1484afe143922a23ae8c2f59c45bc
adnanbajwa <adnan.bajwa163@gmail.com>
2017-05-30 00:59:25 +0300

    add slug in urls, create_slug fucntion

--- django_learning_project/src/posts/models.py
+++ django_learning_project/src/posts/models.py
@@ -36,19 +36,28 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("posts:detail", kwargs={"id": self.id})
+        return reverse("posts:detail", kwargs={"slug": self.slug})
 
     class Meta:
         # In views adding order_by.('-timestamp') also performs the same but
         # its good to add ordering in Meta class.
         ordering = ['-timestamp', '-updated']
 
-def pre_save_post_receiver(sender, instance, *args, **kwargs):
-    #boat in sea = boat-in-sea
+
+def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
-    exists = Post.objects.filter(slug=slug).exists()
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
     if exists:
-        slug = "%s%s" %(slug, instance.id)
-    instance.slug = slug
+        new_slug = "%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
 
 pre_save.connect(pre_save_post_receiver, sender=Post) 
