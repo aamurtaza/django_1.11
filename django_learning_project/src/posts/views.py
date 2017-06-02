b2a0afee4b0704bc74e8ae1268d1ec6b1e50d720
adnanbajwa <adnan.bajwa163@gmail.com>
2017-06-02 18:24:57 +0300

    basic user permissions

--- django_learning_project/src/posts/views.py
+++ django_learning_project/src/posts/views.py
@@ -1,7 +1,7 @@
 from urllib.parse import quote  
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -10,6 +10,8 @@ from .models import Post
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     # If reuqest also submiting files then adding "request.FIlES" is necessary
     form = PostForm(request.POST or None, request.FILES or None)  
     if form.is_valid():
@@ -62,6 +64,8 @@ def post_list(request):
 
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug =slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -82,6 +86,8 @@ def post_update(request, slug=None):
 
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Post successfully deteted")
